f6b0f91f54b22a5c0be9ac98537f78f8bdcd6164
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-09-19 20:06:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+let container = document.createElement('div');
+container.className = 'container';
+for(let i = 0; i <800; i ++){
+	let square = document.createElement('div');
+	square.className = 'square';
+	container.appendChild(square);
+}
+document.body.appendChild(container);
+
+let square = document.querySelectorAll('.square');
+square.forEach(square => {
+	square.addEventListener('mouseover', () =>{
+		square.style.backgroundColor = 'red';
+	});
+});
+square.addEventListener('moverover', () =>{
+	square.style.backgroundColor = 'red';
+	setTimeout (() =>{
+		square.style.backgroundColor = 'white';
+	}, 1000);
+});
+// .square{
+// 	transition : background-color: 1s;
+// }
+
+
+
+
+
