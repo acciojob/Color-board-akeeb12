855fff6e21095a928033cb0c0dfd7b4cdd0a2fb2
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-09-20 08:02:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,11 +7,21 @@ for(let i = 0; i <800; i ++){
 	container.appendChild(square);
 }
 document.body.appendChild(container);
+let squares = document.querySelectorAll('.square');
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 let square = document.querySelectorAll('.square');
 square.forEach(square => {
 	square.addEventListener('mouseover', () =>{
-		square.style.backgroundColor = 'rgb(29,29,29)';
+		square.style.backgroundColor = getRandomColor();
 	setTimeout (() =>{
 		square.style.backgroundColor = 'white';
 	}, 1000);
